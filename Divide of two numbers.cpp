7f5951a1352396bcda1c3c7f11c471a831ff2eee
Narottam Kumar <164016139+NarottamKumar01@users.noreply.github.com>
2025-06-29 17:26:11 +0530

    Add files via upload

---
+++ Divide of two numbers.cpp
@@ -0,0 +1,7 @@
+// write a program to find the divide of two numbers 
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 100 ,b = 25;
+	cout<<a/b;
+};
---
+++ Product of two  number.cpp
@@ -0,0 +1,7 @@
+// write a program to find the product two numbers 
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 100 ,b = 25;
+	cout<<a*b;
+};
